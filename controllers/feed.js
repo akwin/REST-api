ad13acc39301d309fcdba268fdaa78acd4712130
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-21 13:06:19 -0700

    implement fetch posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,17 +3,18 @@ const {validationResult} = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{ 
-            _id: '1',
-            title: 'First Post',
-            content: 'This is the first post.',
-            imageUrl: 'images/poppies.png',
-            creator: {
-                name: 'Akanksha'
-            },
-            createdAt: new Date()
-        }]
+    Post.find()
+    .then(posts => {
+        res.status(200).json(
+            {message: 'Posts fetched successfully', 
+            posts: posts
+        });
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     });
 };
 
@@ -50,3 +51,21 @@ exports.createPost = (req, res, next) => {
     });
 };
 
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('The post was not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({ message: 'Post incoming', post: post });
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
