47a97969fd198c452f05a269885ecf2ef92da169
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-21 16:50:51 -0700

    implement authentication and authorization

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 const {validationResult} = require('express-validator/check');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -46,21 +47,28 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
-    //console.log(title, content);
-    //create post in database
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: { name: 'Akanksha' }
+        creator: req.userId
     });
     post.save()
     .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Your post was created successfully!',
-            post: result
-        });
+        return User.findById(req.userId);
+    })
+    .then(user => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'Your post was created successfully!',
+        post: post,
+        creator: {_id: creator._id, name: creator.name}
+      });
     })
     .catch(err => {
         if (!err.statusCode) {
@@ -115,6 +123,11 @@ exports.updatePost = (req, res, next) => {
           error.statusCode = 404;
           throw error;
         }
+        if (post.creator.toString() !== req.userId) {
+          const error = new Error('This action is unauthorized!');
+          error.statusCode = 403;
+          throw error;
+        }
         if (imageUrl !== post.imageUrl) {
           clearImage(post.imageUrl);
         }
@@ -143,13 +156,24 @@ exports.deletePost = (req, res, next) => {
                 error.statusCode = 404;
                 throw error;
               }
+              if (post.creator.toString() !== req.userId) {
+                const error = new Error('This action is unauthorized!');
+                error.statusCode = 403;
+                throw error;
+              }
             // check logged in user
             clearImage(post.imageUrl);
             return Post.findByIdAndRemove(postId);
         })
         .then(result => {
-            console.log(result);
-            res.status(200).json({message: 'Post deletion successful.'})
+          return User.findById(req.userId);
+        })
+        .then(user => {
+          user.posts.pull(postId);
+          return user.save();
+        })
+        .then(result => {
+          res.status(200).json({message: 'Post deletion successful.'});
         })
         .catch(err => {
             if (!err.statusCode) {
