4cbd57d30a67465af8057a204b7c36b69b720648
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-21 15:09:53 -0700

    implement post update, deletion and pagination

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
+const cors = require('cors');
+
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -38,12 +40,16 @@ app.use(multer({
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});
+  });
 
 app.use('/feed',feedRoutes);
 
