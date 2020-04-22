13d849ff6a7854fb7205c28317359ccfddf215d6
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-21 17:27:25 -0700

    implement status update

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { body }  = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const isAuth = require('../middlware/is-auth');
 
 const User = require('../models/user');
 
@@ -29,5 +30,14 @@ router.put('/signup', [
 ], authController.signup);
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch('/status', isAuth, [
+    body('status')
+        .trim()
+        .not()
+        .isEmpty()
+], authController.updateUserStatus);
 
 module.exports = router;
