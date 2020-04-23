82af9c9d280dcaaf77fadcc9f0424953a64188ba
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-23 00:37:34 -0700

    update package.json

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const graphqlHttp = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 
-const auth = require('./middlware/auth');
+const auth = require('./middleware/auth');
 const { clearImage } = require('./util/file');
 
 const cors = require('cors');
